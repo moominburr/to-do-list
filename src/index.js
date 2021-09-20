90713b719fd76a609c176b1b4bbc6595d62c56f1
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-20 16:09:43 +0100

    Webpack set up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function component() {
+  const element = document.createElement("div");
+
+  
+  element.innerHTML = 'Heya1'
+  element.classList.add("hello");
+
+  return element;
+}
+
+document.body.appendChild(component());
