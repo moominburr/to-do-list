f8c0d733cf695f39f1932e92ca8a2ef7292f70c1
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-20 16:40:30 +0100

    Form basics

--- src/index.js
+++ src/index.js
@@ -16,18 +16,43 @@ const createToDo = ({
   status,
 });
 
-const thursday = createToDo({title : 'Laundry', decription : 'Do laundry', dueDate : '03/01/2022', priority: 2, notes : 'No more clothes', status : 'incomplete'});
+const thursday = createToDo({
+  title: "Laundry",
+  decription: "Do laundry",
+  dueDate: "03/01/2022",
+  priority: 2,
+  notes: "No more clothes",
+  status: "incomplete",
+});
 
 function home() {
-  const element = document.createElement("div");
-  element.innerHTML = "Heya";
-  element.classList.add("hello");
-    for (const thing in thursday) {
-        const p = document.createElement('p');
-        p.innerHTML = thursday[thing];
-        element.appendChild(p);
-    }
-  return element;
+  const container = document.createElement("div");
+  container.classList.add("container");
+  
+  const form = document.createElement("div");
+  
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("id", "title");
+  titleInput.classList.add("title-input");
+  const titleLabel = document.createElement("label");
+  titleLabel.setAttribute("for", "title");
+  titleLabel.innerHTML = "Title: ";
+  
+  const descriptionInput = document.createElement("input");
+  descriptionInput.setAttribute("type", "text");
+  descriptionInput.setAttribute("id", "descr");
+  descriptionInput.classList.add("descr-input");
+  const descrLabel = document.createElement("label");
+  descrLabel.setAttribute("for", "descr");
+  descrLabel.innerHTML = "Description: ";
+  
+  form.appendChild(titleLabel);
+  form.appendChild(titleInput);
+  form.appendChild(descrLabel);
+  form.appendChild(descriptionInput);
+  container.appendChild(form);
+  return container;
 }
 
 document.body.appendChild(home());
