8ff0fedb081d836513f1b9fe666a67ead8533495
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-20 16:27:23 +0100

    Factory function for To Do

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import "./style.css";
+import "./styles/style.css";
 
-function component() {
+const createToDo = ({
+  title = "To Do",
+  description = "What we need to do...",
+  dueDate = "01/01/2022",
+  priority = 1,
+  notes = "Extra info...",
+  status = "incomplete",
+} = {}) => ({
+  title,
+  description,
+  dueDate,
+  priority,
+  notes,
+  status,
+});
+
+const thursday = createToDo({title : 'Laundry', decription : 'Do laundry', dueDate : '03/01/2022', priority: 2, notes : 'No more clothes', status : 'incomplete'});
+
+function home() {
   const element = document.createElement("div");
-
-  
-  element.innerHTML = 'Heya1'
+  element.innerHTML = "Heya";
   element.classList.add("hello");
-
+    for (const thing in thursday) {
+        const p = document.createElement('p');
+        p.innerHTML = thursday[thing];
+        element.appendChild(p);
+    }
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(home());
