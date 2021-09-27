6508892a66cfa46fd0321a0ee918869bd0baecce
Katherine Burr <katherinevb@hotmail.co.uk>
2021-09-27 16:35:14 +0100

    Create to do new file, displaying to dos on page

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
 import "./styles/style.css";
-import form from './todoForm';
+import form from "./todoForm";
+import toDo from "./todo";
 
-const createToDo = ({
-  title = "To Do",
-  description = "What we need to do...",
-  dueDate = "01/01/2022",
-  priority = 1,
-  notes = "Extra info...",
-  status = "incomplete",
-} = {}) => ({
-  title,
-  description,
-  dueDate,
-  priority,
-  notes,
-  status,
-});
+const thursday = new toDo();
+thursday.title = "Water plants";
+thursday.descr = "Water them properly";
+thursday.dueDate = "2022-01-01";
+thursday.priority = 1;
+thursday.notes = "Do something";
+thursday.status = "Not done";
 
-const thursday = createToDo({
-  title: "Laundry",
-  decription: "Do laundry",
-  dueDate: "03/01/2022",
-  priority: 2,
-  notes: "No more clothes",
-  status: "incomplete",
-});
 
 function home() {
   const container = document.createElement("div");
   container.classList.add("container");
-  
-  
-  
-  
+
+  const toDoList = [];
+  toDoList.push(thursday);
+
+  const toDoContainer = document.createElement("div");
+
+  toDoList.forEach((el) => {
+    const toDo = document.createElement("div");
+    toDo.classList.add('to-do-individual');
+    for (let prop in el) {
+      let p;
+      if (prop === 'title') {
+        p = document.createElement("h2");
+      } else {
+        p = document.createElement("p");
+      }
+
+      p.innerText = el[prop];
+      toDo.appendChild(p);
+    }
+    toDoContainer.appendChild(toDo);
+  });
+
+  container.appendChild(toDoContainer);
   container.appendChild(form());
   return container;
 }
--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,13 @@
 
+export default class toDo{
+    constructor(title, descr, dueDate, priority, notes, status){
+        this.title = title;
+        this.descr = descr;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.status = status;
+    }
+}
+
+
